a0c87a9a7a0b2459ce88c5acbc6db4ae2f301c07
chenghaol1 <lich2000117@gmail.com>
2022-10-20 22:35:27 +1100

    add fast move VM

--- CopyToSFs.bat
+++ CopyToSFs.bat
@@ -7,11 +7,11 @@ cd /d "C:\Users\Lee\Desktop\sf4"
 rmdir "." /s /q
 cd /d "%olddir%"
 
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\client-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\SharedFolder2\" /y
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\client-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\sf3\" /y
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\client-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\sf4\" /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\SharedFolder2\" /s /e /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\sf3\" /s /e /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\sf4\" /s /e /y
 
 
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\server-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\SharedFolder2\" /y
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\server-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\sf3\" /y
-xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard\target\server-jar-with-dependencies.jar" "C:\Users\Lee\Desktop\sf4\" /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\SharedFolder2\" /s /e /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\sf3\" /s /e /y
+xcopy "G:\SharedFolder_Ubuntu\OnlineWhiteBoard" "C:\Users\Lee\Desktop\sf4\" /s /e /y
